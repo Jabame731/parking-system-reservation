cb95df90e5d0f83b41cfb06a7a031db827a5abbe
Jabame731 <justinetaglucop@gmail.com>
2025-12-28 12:34:30 +0800

    setting up the backend express typescript using SOLID

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.listen(8800, () => {
+  console.log(`Server is running on Port ${8800}`);
+});
+
+export default app;
+
---
+++ backend/src/utils/models/user.model.ts
@@ -0,0 +1,18 @@
+export interface User {
+  id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  password: string;
+  address: string;
+  country: string;
+  city: string;
+  contactNumber: string;
+  role: string;
+  token: string;
+  createdAt: string;
+  updatedAt: string;
+  refreshToken: string;
+}
