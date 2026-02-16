92cae7cb073bd1af6c2c66f69f512216e6bc510f
Jabame731 <justinetaglucop@gmail.com>
2026-02-16 21:00:19 +0800

    change env

--- frontend/src/parking-system-store/src/environments/environment.production.ts
+++ frontend/src/parking-system-store/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://parking-system-reservation.vercel.app/',
+  apiBaseUrl: 'https://parking-system-reservation.vercel.app',
   paypalClientId:
     'AUSsTNzLQMheq9a--SK2XR9dmmVbZ8OmQILMoVbRnJJ_7plpmiKTbkZqDpk7O3IOQaG6K0CHFXsy-P1-',
 };
